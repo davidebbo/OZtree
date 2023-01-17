c2c6bd02dd5501d004cb68331eeae755062b4d6e
David Ebbo <david.ebbo@gmail.com>
2023-01-16 20:30:09 -0800

    Process all entries

--- OZprivate/ServerScripts/Utilities/extract_minimal_tree.py
+++ OZprivate/ServerScripts/Utilities/extract_minimal_tree.py
@@ -5,27 +5,26 @@ parser = argparse.ArgumentParser(description=__doc__)
 parser.add_argument(
     "Tree_File", type=argparse.FileType('r'), help="A tree file in newick form")
 args = parser.parse_args()
-taxas = ["Vicugna"]
-taxas = ["Primates"]
-taxas = ["Camelidae"]
-taxas = ["Piliocolobus_badius"]
-taxas = ["Bad"]
-taxas = ["Homininae"]
-taxas = ["Vicugna","Pan_paniscus"]
-taxas = ["Ochotona_curzoniae","Pan_paniscus"]
+taxa = ["Vicugna"]
+taxa = ["Primates"]
+taxa = ["Camelidae"]
+taxa = ["Piliocolobus_badius"]
+taxa = ["Bad"]
+taxa = ["Homininae"]
+taxa = ["Vicugna","Pan_paniscus"]
+taxa = ["Sylvilagus_nuttallii", "Trichechus_manatus", "Pan_paniscus", "Canis_lupus", "Ornithorhynchus_anatinus", "Discoglossus_montalentii", "Heterodontus_japonicus"]
 
 tree = args.Tree_File.read()
-nodes = {taxa:{"depth":-1} for taxa in taxas}
+
+nodes = []
 
 index = 0
 index_stack = []
 
-# Camelus_ott510767:9.464505
-
 whole_token_regex = re.compile('[^(),;]*')
 taxon_regex = re.compile('^(\w*)_ott\d*(:[\d\.]*)?')
 
-while True:
+while taxa or len(nodes) >= 2:
     if index == len(tree) or tree[index] == ';':
         break
 
@@ -47,22 +46,23 @@ while True:
     match_taxon = taxon_regex.match(match_full_name.group())
     if (match_taxon):
         taxon = match_taxon.group(1)
-        if taxon in nodes:
-            nodes[taxon]["tree_string"] = tree[start_index:index]
-            nodes[taxon]["depth"] = len(index_stack)
-            print(f"Found {taxon} at depth {len(index_stack)} and position {index}")
-            print(nodes[taxon]["tree_string"])
+        if taxon in taxa:
+            taxa.remove(taxon)
+            nodes.append({"tree_string": tree[start_index:index], "depth": len(index_stack)})
 
     if closed_brace:
-        lowered = []
-        for node in nodes.items():
-            if node[1]["depth"] == len(index_stack)+1:
-                node[1]["depth"] -= 1
-                lowered.append(node)
-                pass
+        lowered = [n for n in nodes if n["depth"] == len(index_stack)+1]
+        for node in lowered:
+            node["depth"] -= 1
+            
         if len(lowered) > 1:
-            print(f"({lowered[0][1]['tree_string']},{lowered[1][1]['tree_string']}){match_full_name.group()}")
-            break
+            nodes = [n for n in nodes if n not in lowered]
+            nodes.append({"tree_string": f"({lowered[0]['tree_string']},{lowered[1]['tree_string']}){match_full_name.group()}", "depth": len(index_stack)})
 
     if tree[index] == ',':
         index += 1
+
+if taxa:
+    print(f"Could not find target taxon '{taxa[0]}' in the tree")
+else:
+    print(nodes[0]['tree_string'] + ";")
