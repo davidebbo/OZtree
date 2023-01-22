5fe19609c777610f9cc4315924f1cd8057935682
David Ebbo <david.ebbo@gmail.com>
2023-01-22 11:02:40 -0800

    Misc extract_minimal cleanup

--- OZprivate/ServerScripts/Utilities/extract_minimal_tree.py
+++ OZprivate/ServerScripts/Utilities/extract_minimal_tree.py
@@ -1,24 +1,29 @@
+#!/usr/bin/env python3
+
 import argparse
+import sys
 import re
 
 parser = argparse.ArgumentParser(description=__doc__)
-parser.add_argument(
-    "Tree_File", type=argparse.FileType('r'), help="A tree file in newick form")
+parser.add_argument('treefile', type=argparse.FileType('r'), nargs='?', default=sys.stdin, help='The tree file in newick form')
 args = parser.parse_args()
+
+
 taxa = ["Primates"]
 taxa = ["Piliocolobus_badius"]
 taxa = ["Homininae"]
 taxa = ["Vicugna", "Pan_paniscus"]
 taxa = ["Camelidae"]
 taxa = ["Bad"]
-taxa = ["Sylvilagus_nuttallii", "Trichechus_manatus", "Pan_paniscus", "Canis_lupus",
-        "Ornithorhynchus_anatinus", "Discoglossus_montalentii", "Heterodontus_japonicus"]
 taxa = ["Vicugna", "Homininae"]
 taxa = ["Rodentia", "Lagomorpha", "Primates", "Dermoptera", "Tupaia"]
 taxa = ["Glires", "Primates", "Scandentia"]
 taxa = ["Rodentia", "Lagomorpha", "Primates", "Dermoptera", "Tupaia"]
+taxa = ["Sylvilagus_nuttallii", "Trichechus_manatus", "Pan_ppaniscus", "Canis_lupus",
+        "Ornithorhynchus_anatinus", "Discoglossus_montalentii", "Heterodontus_japonicus"]
+taxa = ["Tupaia_chrysogaster", "Tupaia_belangeri", "Canis_lupus", "Tupaia_montana", "Tupaia_moellendorffi"]
 
-tree = args.Tree_File.read()
+tree = args.treefile.read()
 
 # We build the node list as we find them and process them
 nodes = []
@@ -28,6 +33,8 @@ index_stack = []
 
 whole_token_regex = re.compile('[^(),;]*')
 taxon_regex = re.compile('^(\w*)_ott\d*(:[\d\.]*)?')
+
+taxon_count=0
 
 while taxa or len(nodes) >= 2:
     if index == len(tree) or tree[index] == ';':
@@ -42,7 +49,7 @@ while taxa or len(nodes) >= 2:
     if closed_brace:
         index += 1
         start_index = index_stack.pop()
-        # start_index = index
+        start_index = index
     else:
         start_index = index
 
@@ -51,6 +58,8 @@ while taxa or len(nodes) >= 2:
 
     match_taxon = taxon_regex.match(match_full_name.group())
     if (match_taxon):
+        # taxon_count += 1
+
         taxon = match_taxon.group(1)
         if taxon in taxa:
             # We've found a taxon, so remove it from the list, and create a node for it
@@ -64,11 +73,14 @@ while taxa or len(nodes) >= 2:
             node["depth"] -= 1
 
         if len(nodes_with_current_depth) > 1:
-            # We've found at least two nodes that match the current depth, so remove them and wrap them in a new node
-            # TODO: handle polytomies when creating the new node
+            # We've found at least two nodes that match the current depth
+
+            # Remove them from the list of nodes we're looking for
             nodes = [n for n in nodes if n not in nodes_with_current_depth]
+
+            # Replace them by a new node that wraps them
             nodes.append(
-                {"tree_string": f"({nodes_with_current_depth[0]['tree_string']},{nodes_with_current_depth[1]['tree_string']}){match_full_name.group()}",
+                {"tree_string": f"({','.join([node['tree_string'] for node in nodes_with_current_depth])}){match_full_name.group()}",
                 "depth": len(index_stack)})
 
     if tree[index] == ',':
@@ -78,3 +90,5 @@ if taxa:
     print(f"Could not find target taxon '{taxa[0]}' in the tree")
 else:
     print(nodes[0]['tree_string'] + ";")
+
+print(f"Taxon count: {taxon_count}")
